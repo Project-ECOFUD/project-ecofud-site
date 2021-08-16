80dd3d6a3a3bdd26bda23fdb7e6a4e04c9885017
Will O'Beirne <wbobeirne@gmail.com>
2021-08-15 21:46:20 -0500

    Fix commaify on decimals

--- util.js
+++ util.js
@@ -1,3 +1,5 @@
 export function commaify(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  const parts = x.toString().split(".");
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return parts.join(".");
 }
